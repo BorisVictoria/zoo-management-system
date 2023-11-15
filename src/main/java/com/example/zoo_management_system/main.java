9da7bed344a2b607f66e09fd45e120fbbf28f1f4
Boris Victoria <boris.v.victoria@gmail.com>
2023-11-15 17:44:58 +0800

    Best zoo management system ever!

--- src/main/java/com/example/zoo_management_system/main.java
+++ src/main/java/com/example/zoo_management_system/main.java
@@ -16,7 +16,7 @@ public class main extends Application {
         loader.setControllerFactory(controllerClass -> new mainController());
         Parent root = loader.load();
         Scene scene = new Scene(root, 600, 500);
-        stage.setTitle("Zoo Management System");
+        stage.setTitle("Zoo Management System 1");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
