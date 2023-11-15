418c836567308c55094f7d69744d54773d963d16
Boris Victoria <boris.v.victoria@gmail.com>
2023-11-15 17:44:12 +0800

    Best zoo management system ever!

---
+++ src/main/java/com/example/zoo_management_system/main.java
@@ -0,0 +1,28 @@
+package com.example.zoo_management_system;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+        loader.setControllerFactory(controllerClass -> new mainController());
+        Parent root = loader.load();
+        Scene scene = new Scene(root, 600, 500);
+        stage.setTitle("Zoo Management System");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/zoo_management_system/mainController.java
@@ -0,0 +1,14 @@
+package com.example.zoo_management_system;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class mainController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
